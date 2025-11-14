540dee599cb6415c415cbe2c059fbef2001c0d23
Dafoe17 <ram.and.rem207@gmail.com>
2025-11-14 12:31:50 +0300

    feat(deals/api): add new Get endpoints for searching by created_at, updated_at and closed_at

--- src/api/deals.py
+++ src/api/deals.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends, Query
 from src.api.dependencies import Session, get_db, require_roles
 
+from datetime import datetime, timedelta, timezone
 from src.enums import SortOrder
 from src.models import User, Client, Deal
 from src.schemas.deal import DealRead, DealCreate, DealsListResponse, StatusDealsResponse
@@ -9,18 +10,18 @@ router = APIRouter(tags=['Deals'])
 
 @router.get("/deals/get-all", response_model=DealsListResponse, operation_id="get-all-deals")
 async def get_all_deals(db: Session = Depends(get_db), 
-                          current_user: User = Depends(require_roles('admin', 'manager')),
-                          skip: int = Query(None, description="Number of deals to skip"),
-                          limit: int = Query(None, description="Number of deals to return"),
-                          search: str | None = Query(None, description="Search by title"),
-                          bigger_than: int = Query(None, description="Filter deals with value bigger than arg"),
-                          less_than: int = Query(None, description="Filter deals with value less than arg"),
-                          related_to_curr_user: bool = Query(False, description="Filter deals related to to your user"),
-                          related_to_user: str | None = Query(None, description="Filter deals related to user"),
-                          related_to_client: str | None = Query(None, description="Filter deals related to clients"),
-                          sort_by: str = Query("id", description="Sort by field: id, title, status, value"),
-                          order: SortOrder = Query("asc", description="Sort order: asc or desc"),
-                          ) -> DealsListResponse:
+                        current_user: User = Depends(require_roles('admin', 'manager')),
+                        skip: int = Query(None, description="Number of deals to skip"),
+                        limit: int = Query(None, description="Number of deals to return"),
+                        search: str | None = Query(None, description="Search by title"),
+                        bigger_than: int = Query(None, description="Filter deals with value bigger than arg"),
+                        less_than: int = Query(None, description="Filter deals with value less than arg"),
+                        related_to_curr_user: bool = Query(False, description="Filter deals related to to your user"),
+                        related_to_user: str | None = Query(None, description="Filter deals related to user"),
+                        related_to_client: str | None = Query(None, description="Filter deals related to clients"),
+                        sort_by: str = Query("id", description="Sort by field: id, title, status, value"),
+                        order: SortOrder = Query("asc", description="Sort order: asc or desc"),
+                        ) -> DealsListResponse:
     
     query = db.query(Deal)
 
@@ -60,7 +61,224 @@ async def get_all_deals(db: Session = Depends(get_db),
         total=total_deals,
         skip=skip,
         limit=limit,
-        clients=[Deal.model_validate(deal) for deal in deals]
+        deals=[Deal.model_validate(deal) for deal in deals]
+    )
+
+@router.get("/deals/get-by-created-date", response_model=DealsListResponse, operation_id="get-by-created-date")
+async def get_by_create_date(db: Session = Depends(get_db),
+                            current_user: User = Depends(require_roles('admin', 'manager')),
+                            skip: int = Query(None, description="Number of deals to skip"),
+                            limit: int = Query(None, description="Number of deals to return"),
+                            exact_date: datetime | None = Query(None, description="Search by exact day"),
+                            earlier_than: datetime | None = Query(None, description="Filter deals created earlier than arg"),
+                            later_than: datetime | None = Query(None, description="Filter deals created later than arg"),
+                            new: bool = Query(False, description="Filter deals created earlier this mounth"),
+                            related_to_curr_user: bool = Query(False, description="Filter deals related to to your user"),
+                            related_to_user: str | None = Query(None, description="Filter deals related to user"),
+                            related_to_client: str | None = Query(None, description="Filter deals related to clients"),
+                            sort_by: str = Query("id", description="Sort by field: id, title, status, value"),
+                            order: SortOrder = Query("asc", description="Sort order: asc or desc"),
+                            ) -> DealsListResponse:
+    query = db.query(Deal)
+
+    if related_to_client:
+        client_ids = db.query(Client.id).filter(Client.name.islike(f"%{related_to_client}%")).subquery()
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if related_to_user:
+        user_ids = db.query(User.id).filter(User.username.islike(f"%{related_to_user}%")).subquery
+        client_ids = db.query(Client.id).filter(Client.user_id.in_(user_ids))
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if related_to_curr_user:
+        user_ids = db.query(User.id).filter(User.username.islike(f"%{current_user.username}%")).subquery
+        client_ids = db.query(Client.id).filter(Client.user_id.in_(user_ids))
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if exact_date:
+        start_of_day = datetime.combine(exact_date.date(), datetime.min.time())
+        end_of_day = datetime.combine(exact_date.date(), datetime.max.time())
+        query = query.filter(Deal.created_at >= start_of_day, Deal.created_at <= end_of_day)
+
+    if earlier_than:
+        query = query.filter(Deal.created_at >= earlier_than)
+    
+    if later_than:
+        query = query.filter(Deal.created_at >= later_than)
+
+    if new:
+        today = datetime.today()
+        start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        if today.month == 12:
+            next_month = today.replace(year=today.year+1, month=1, day=1)
+        else:
+            next_month = today.replace(month=today.month+1, day=1)
+        end_of_month = next_month - timedelta(seconds=1)
+
+        query = query.filter(Deal.created_at >= start_of_month, Deal.created_at <= end_of_month)
+    
+    if hasattr(Deal, sort_by):
+        sort_attr = getattr(Deal, sort_by)
+        query = query.order_by(sort_attr.desc() if order.lower() == "desc" else sort_attr.asc())
+    else:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field: {sort_by}")
+
+    total_deals = query.count()
+    deals = query.offset(skip).limit(limit).all()
+
+    return DealsListResponse(
+        total=total_deals,
+        skip=skip,
+        limit=limit,
+        deals=[DealRead.model_validate(deal) for deal in deals]
+    )
+
+@router.get("/deals/get-by-updated-date", response_model=DealsListResponse, operation_id="get-by-updated-date")
+async def get_by_update_date(db: Session = Depends(get_db),
+                            current_user: User = Depends(require_roles('admin', 'manager')),
+                            skip: int = Query(None, description="Number of deals to skip"),
+                            limit: int = Query(None, description="Number of deals to return"),
+                            exact_date: datetime | None = Query(None, description="Search by exact day"),
+                            earlier_than: datetime | None = Query(None, description="Filter deals updated earlier than arg"),
+                            later_than: datetime | None = Query(None, description="Filter deals updated later than arg"),
+                            new: bool = Query(False, description="Filter deals updated earlier this mounth"),
+                            related_to_curr_user: bool = Query(False, description="Filter deals related to to your user"),
+                            related_to_user: str | None = Query(None, description="Filter deals related to user"),
+                            related_to_client: str | None = Query(None, description="Filter deals related to clients"),
+                            sort_by: str = Query("id", description="Sort by field: id, title, status, value"),
+                            order: SortOrder = Query("asc", description="Sort order: asc or desc"),
+                            ) -> DealsListResponse:
+    
+    query = db.query(Deal)
+
+    if related_to_client:
+        client_ids = db.query(Client.id).filter(Client.name.islike(f"%{related_to_client}%")).subquery()
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if related_to_user:
+        user_ids = db.query(User.id).filter(User.username.islike(f"%{related_to_user}%")).subquery
+        client_ids = db.query(Client.id).filter(Client.user_id.in_(user_ids))
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if related_to_curr_user:
+        user_ids = db.query(User.id).filter(User.username.islike(f"%{current_user.username}%")).subquery
+        client_ids = db.query(Client.id).filter(Client.user_id.in_(user_ids))
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if exact_date:
+        start_of_day = datetime.combine(exact_date.date(), datetime.min.time())
+        end_of_day = datetime.combine(exact_date.date(), datetime.max.time())
+        query = query.filter(Deal.updated_at >= start_of_day, Deal.updated_at <= end_of_day)
+
+    if earlier_than:
+        query = query.filter(Deal.updated_at >= earlier_than)
+    
+    if later_than:
+        query = query.filter(Deal.updated_at >= later_than)
+
+    if new:
+        today = datetime.today()
+        start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        if today.month == 12:
+            next_month = today.replace(year=today.year+1, month=1, day=1)
+        else:
+            next_month = today.replace(month=today.month+1, day=1)
+        end_of_month = next_month - timedelta(seconds=1)
+
+        query = query.filter(Deal.updated_at >= start_of_month, Deal.updated_at <= end_of_month)
+    
+    if hasattr(Deal, sort_by):
+        sort_attr = getattr(Deal, sort_by)
+        query = query.order_by(sort_attr.desc() if order.lower() == "desc" else sort_attr.asc())
+    else:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field: {sort_by}")
+
+    total_deals = query.count()
+    deals = query.offset(skip).limit(limit).all()
+
+    return DealsListResponse(
+        total=total_deals,
+        skip=skip,
+        limit=limit,
+        deals=[DealRead.model_validate(deal) for deal in deals]
+    )
+
+@router.get("/deals/get-by-closed-date", response_model=DealsListResponse, operation_id="get-by-closed-date")
+async def get_by_closed_date(db: Session = Depends(get_db),
+                            current_user: User = Depends(require_roles('admin', 'manager')),
+                            skip: int = Query(None, description="Number of deals to skip"),
+                            limit: int = Query(None, description="Number of deals to return"),
+                            exact_date: datetime | None = Query(None, description="Search by exact day"),
+                            earlier_than: datetime | None = Query(None, description="Filter deals closed earlier than arg"),
+                            later_than: datetime | None = Query(None, description="Filter deals closed later than arg"),
+                            soon: bool = Query(False, description="Filter deals closed this mounth"),
+                            expired: bool = Query(False, description="Filter closed deals"),
+                            related_to_curr_user: bool = Query(False, description="Filter deals related to to your user"),
+                            related_to_user: str | None = Query(None, description="Filter deals related to user"),
+                            related_to_client: str | None = Query(None, description="Filter deals related to clients"),
+                            sort_by: str = Query("id", description="Sort by field: id, title, status, value"),
+                            order: SortOrder = Query("asc", description="Sort order: asc or desc"),
+                            ) -> DealsListResponse:
+    
+    query = db.query(Deal)
+
+    if related_to_client:
+        client_ids = db.query(Client.id).filter(Client.name.islike(f"%{related_to_client}%")).subquery()
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if related_to_user:
+        user_ids = db.query(User.id).filter(User.username.islike(f"%{related_to_user}%")).subquery
+        client_ids = db.query(Client.id).filter(Client.user_id.in_(user_ids))
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if related_to_curr_user:
+        user_ids = db.query(User.id).filter(User.username.islike(f"%{current_user.username}%")).subquery
+        client_ids = db.query(Client.id).filter(Client.user_id.in_(user_ids))
+        query = query.filter(Deal.client_id.in_(client_ids))
+
+    if exact_date:
+        start_of_day = datetime.combine(exact_date.date(), datetime.min.time())
+        end_of_day = datetime.combine(exact_date.date(), datetime.max.time())
+        query = query.filter(Deal.closed_at >= start_of_day, Deal.closed_at <= end_of_day)
+
+    if earlier_than:
+        query = query.filter(Deal.closed_at >= earlier_than)
+    
+    if later_than:
+        query = query.filter(Deal.closed_at >= later_than)
+
+    if soon:
+        today = datetime.today()
+        start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        
+        if today.month == 12:
+            next_month = today.replace(year=today.year + 1, month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+        else:
+            next_month = today.replace(month=today.month + 1, day=1, hour=0, minute=0, second=0, microsecond=0)
+        
+        end_of_month = next_month - timedelta(microseconds=1)
+        query = query.filter(Deal.closed_at >= start_of_month, Deal.closed_at <= end_of_month)
+        
+        if hasattr(Deal, sort_by):
+            sort_attr = getattr(Deal, sort_by)
+            query = query.order_by(sort_attr.desc() if order.lower() == "desc" else sort_attr.asc())
+        else:
+            raise HTTPException(status_code=400, detail=f"Invalid sort field: {sort_by}")
+        
+    if expired:
+        now = datetime.now(timezone.utc)  
+        query = query.filter(Deal.closed_at <= now)
+
+    total_deals = query.count()
+    deals = query.offset(skip).limit(limit).all()
+
+    return DealsListResponse(
+        total=total_deals,
+        skip=skip,
+        limit=limit,
+        deals=[DealRead.model_validate(deal) for deal in deals]
     )
 
 @router.post("/deals/add", response_model=StatusDealsResponse, operation_id="add-deal")
