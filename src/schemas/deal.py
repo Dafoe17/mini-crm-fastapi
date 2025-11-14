b4434bb4b18bd2a7c8a234da103222c82fb6e1ba
Dafoe17 <ram.and.rem207@gmail.com>
2025-11-14 11:45:00 +0300

    feat(deal/schemas): refactoring classes, now with auto-filling create_at and new atrribute update_at. New validations for closed_time after today. client_id to client_name for clarity.

--- src/schemas/deal.py
+++ src/schemas/deal.py
@@ -1,39 +1,51 @@
 from pydantic import BaseModel, ConfigDict, Field, field_validator
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional, List, Union
 from src.enums import DealStatus, ActionStatus
 
 
 class DealBase(BaseModel):
-    client_id: int = Field(gt=0)
     title: str = Field(max_length=50, min_length=2, json_schema_extra={"strip_whitespace": True})
     status: DealStatus
     value: int = Field(gt=0)
-    created_at: datetime
-    closed_at: datetime | None
+    closed_at: datetime | None = None
 
     @field_validator("title")
     def strip_title(cls, v):
         return v.strip()
+    
+    @field_validator("closed_at")
+    def closed_at_not_in_past(cls, value):
+        if value is None:
+            return value
+
+        if value.tzinfo is None:
+            value = value.replace(tzinfo=timezone.utc)
+
+        if value <= datetime.now(timezone.utc):
+            raise ValueError("closed_at must be in the future")
+        return value
 
 class DealRead(DealBase):
+    created_at: datetime
+    updated_at: datetime
     id: int
 
     model_config = ConfigDict(from_attributes=True)
 
 class DealCreate(DealBase):
-    pass
+    client_name: Optional[str] = None
 
 class DealUpdate(DealBase):
     model_config = ConfigDict(partial=True)
 
 class StatusDealsResponse(BaseModel):
     status: ActionStatus
-    clients: Optional[Union[List[DealRead], DealRead]] = None
+    deals: Optional[Union[List[DealRead], DealRead]] = None
     details: Optional[str] = None
 
 class DealsListResponse(BaseModel):
     total: int = Field(ge=0)
     skip: Optional[int] = Field(default=None, ge=0)
     limit: Optional[int] = Field(default=None, ge=0)
-    clients: Optional[Union[List[DealRead], DealRead]] = None
+    deals: Optional[Union[List[DealRead], DealRead]] = None
